c87642293963075ac75b9167edb073db5013663e
ovo.code <onufriychuk.vo@yandex.ru>
2023-10-02 04:06:15 +0500

    _

--- src/main/java/ru/otus/java/basic/homework11/animals/Dog.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.java.basic.homework11.animals;
-
-public class Dog extends Animal {
-    public Dog(String name, int speedRun, int speedSwim, int stamina, int runStaminaPerMetre, int swimStaminaPerMetre) {
-        super(name, speedRun, speedSwim, stamina);
-        this.runStaminaPerMetre = runStaminaPerMetre;
-        this.swimStaminaPerMetre = swimStaminaPerMetre;
-    }
-}
