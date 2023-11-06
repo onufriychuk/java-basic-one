e4a44b0b5325cc573a285ebbe5590dc739edab43
ovo.code <onufriychuk.vo@yandex.ru>
2023-11-06 14:30:58 +0500

    дз по теме 18. Java IO. Часть 1

---
+++ src/main/java/ru/otus/java/basic/homework17/Main.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homework17;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/main/java/ru/otus/java/basic/homework18/Main.java
+++ src/main/java/ru/otus/java/basic/homework18/Main.java
@@ -1,6 +1,7 @@
 package ru.otus.java.basic.homework18;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,17 +17,30 @@ public class Main {
             }
         }
 
-        System.out.println("Какой файл вы хотели бы отредактировать? Введите название");
         Scanner scanner = new Scanner(System.in);
-        String currentfile = folder + "/" + scanner.nextLine();
-        File checkedFile = new File(currentfile);
+
+        System.out.println("Какой файл вы хотели бы отредактировать? Введите название");
+        String userFile = folder + "/" + scanner.nextLine();
+        File checkedFile = new File(userFile);
         try {
             FileInputStream fileInputStream = new FileInputStream(checkedFile.getPath());
             int i;
-            while ((i=fileInputStream.read())!=-1) {
-                System.out.print((char)i);
+            while ((i = fileInputStream.read()) != -1) {
+                System.out.print((char) i);
             }
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Введите строку для добавления в файл");
+        String writeData = scanner.nextLine();
+        writeData = writeData + "\n";
+        try {
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(checkedFile, true));
+            byte[] buffer = writeData.getBytes(StandardCharsets.UTF_8);
+            out.write(buffer);
+            out.flush();
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             scanner.close();
